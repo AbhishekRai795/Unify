04ce2666290d28a885f067d7190c42cfdf9c6313
govindheda070504 <govindmaheshwari397@gmail.com>
2026-04-21 17:26:31 +0530

    feat: consolidate backend functions into deploy/functions and update template.yaml

--- scratch.js
+++
@@ -1,6 +0,0 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
-const client = DynamoDBDocumentClient.from(new DynamoDBClient({ region: "ap-south-1" }));
-client.send(new ScanCommand({ TableName: "Certificates" }))
-  .then(res => console.log(JSON.stringify(res.Items, null, 2)))
-  .catch(err => console.error(err));
